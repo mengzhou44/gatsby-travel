7a16b1d427bbab1d4aead1452775da41f50ea5a2
Meng Zhou <v-menzhou@expediagroup.com>
2022-06-16 16:41:01 -0500

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React from "react"
-
 import Layout from "../components/Layout"
 import SearchEngineOptimization from "../components/SearchEngineOptimization"
-import Testimonials from "../components/home/Testimonials"
 import Hero from "../components/home/Hero"
 import Trips from "../components/home/Trips"
 import Stats from "../components/home/Stats"
 import Email from "../components/home/Email"
+import Testimonials from "../components/home/Testimonials"
 
 const IndexPage = () => (
   <Layout isHomePage={true}>
