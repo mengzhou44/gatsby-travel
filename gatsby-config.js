570475b7f839e95619b0cd20f007408e3d55e5c8
Meng Zhou <v-menzhou@expediagroup.com>
2022-06-19 09:36:46 -0500

    offline mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,5 +25,11 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/*`],
+      },
+    },
   ],
 }
